e0eb0f4e9cfc176c3864cb721bd53566f6cb40a8
Carlos Antônio <carlos.bezerra12@outlook.com>
2025-01-03 13:57:20 -0300

    added csv functions and interface

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
+from tkinter import *
+from tkinter import ttk
 import pyautogui as pg
 import time
-
-print('qual o nome do contato para mandar a mensagem?')
-contact = input()
-print('Qual a mensagem?')
-M = input()
-print('digite Y para continuar')
-C = input()
-
-if C == 'Y' or 'y':
-    #aperta a tecla windows do teclado e entra no whatsapp
-    pg.press('win')
-    pg.write('whatsApp', interval=0.05)
-    pg.press('enter')
-    #pesquisa o contato
-    time.sleep(2.5)
-    pg.hotkey('ctrl' + 'F')
-    pg.write(contact)
-    #seleciona o contato
-    pg.press('down')
-    pg.press('enter')
-    #manda a mensagem
-    time.sleep(2)
-    pg.click(x=1100, y=950)
-    pg.write(M, interval=0.2)
-    pg.press('enter')
+import csv
+import pandas as pd
+        
+root = Tk()
+root.title("Zabot")
+root.minsize(800, 600)
+label = ttk.Label(root, text="Bem vindo ao Zabot!", font=("Helvetica", 20), justify=CENTER)
+label.pack()
+label2 = ttk.Label(root, text="digite o nome do contato que queres mandar a mensagem abaixo", font=("Helvetica", 10), justify=CENTER)
+label2.pack()
+Imp = ttk.Entry(root)
+Imp.pack()
+label3 = ttk.Label(root, text="agora, digite a mensagem que deseja enviar", font=("Helvetica", 10), justify=CENTER)
+label3.pack()
+Imp2 = ttk.Entry(root)
+Imp2.pack()
+def data():
+    global cont 
+    cont = Imp.get()
+    global M
+    M = Imp2.get()
+    with open('CeM.csv', 'a') as f:
+        w = csv.writer(f,dialect='excel-tab')
+        w.writerow([cont, M])
+botao = ttk.Button(root, text="salvar para enviar", command=data)
+botao.pack()
+botao = ttk.Button(root, text="Clique aqui para finalizar o envio", command=root.destroy)
+botao.pack()
+root.mainloop()
+pg.press('win')
+pg.write('whatsApp', interval=0.08)
+pg.press('enter')
+#pesquisa o contato
+time.sleep(2.8)
+pg.hotkey('ctrl' + 'F')
+pg.write(cont)
+#seleciona o contato
+pg.press('down')
+pg.press('enter')
+#manda a mensagem
+time.sleep(2)
+pg.click(x=1100, y=950)
+pg.write(M, interval=0.15)
+pg.press('enter')
 
